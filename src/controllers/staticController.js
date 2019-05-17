0a7c4b0436b0415593a9c3f5d582d65f4d774bba
Carla <carla.f.bates@hotmail.com>
2019-05-17 15:14:56 -0400

    issue 11 complete

---
+++ src/controllers/staticController.js
@@ -0,0 +1,16 @@
+// const unirest = require('unirest');
+
+module.exports = {
+    index(req, res, next) {
+        res.render("static/index", { title: "What Should I Eat?" });
+    }
+}
+
+
+
+// unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=apples%2Cflour%2Csugar")
+// .header("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
+// .header("X-RapidAPI-Key", "a509516b83msh593f961864e03c9p171205jsn484cbe1ef741")
+// .end(function (result) {
+//   console.log(result.status, result.headers, result.body);
+// });
--- src/routes/static.js
+++ src/routes/static.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    res.send("What Should I Eat?");
-});
+const staticController = require("../controllers/staticController");
+router.get("/", staticController.index);
+
 
 module.exports = router;
